1ac17a5e51c760a54b895e16a091ae0e67c098e8
gennadyalheev <soorov2013@gmail.com>
2021-09-19 00:22:23 +0300

    Поправил импорт файла

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:demhack3_web/Pages/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:demhack3_web/fingerprint_loader.dart' as FingerPrint;
 
 void main() {
   runApp(const MyApp());
@@ -21,9 +22,20 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  void initState() {
+    super.initState();
+    FingerPrint.getBrowserFingerPrint().then((value) => print(FingerPrint.stringify(value)));
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
